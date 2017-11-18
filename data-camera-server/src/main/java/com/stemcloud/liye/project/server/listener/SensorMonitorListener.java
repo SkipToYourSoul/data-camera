f136d60ea8ead827f6ca1b3284f469c9e517f105
李叶 <liye@qiyi.com>
2017-11-18 18:32:46 +0800

    add dc-server

---
+++ data-camera-server/src/main/java/com/stemcloud/liye/project/server/listener/SensorMonitorListener.java
@@ -0,0 +1,10 @@
+package com.stemcloud.liye.project.server.listener;
+
+/**
+ * Belongs to data-camera-server
+ * Description:
+ *  listen sensor monitor status
+ * @author liye on 2017/11/18
+ */
+public class SensorMonitorListener {
+}
