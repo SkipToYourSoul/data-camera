c06cf9b43824c3b86ec47578eea93f2c623d0a97
browser <785094749@qq.com>
2018-01-30 18:39:28 +0800

    少删一行空格

--- data-camera-web/src/main/java/com/stemcloud/liye/dc/service/CrudService.java
+++ data-camera-web/src/main/java/com/stemcloud/liye/dc/service/CrudService.java
@@ -15,7 +15,6 @@ import com.stemcloud.liye.dc.domain.base.TrackInfo;
 import com.stemcloud.liye.dc.domain.config.SensorRegister;
 import com.stemcloud.liye.dc.domain.data.ContentInfo;
 import com.stemcloud.liye.dc.domain.data.RecorderInfo;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
